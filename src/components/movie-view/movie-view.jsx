1faf7d2c5d23430921b6a38f3be7d995b08588f7
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-11-10 10:36:50 -0500

    Created Movie-View component

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,19 @@
+export const MovieView = ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <div>
+      <span>Description: </span>
+      <span>{movie.description}</span>
+    </div>
+    <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
