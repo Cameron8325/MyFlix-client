5ffb18b5bdab4cc89b5d63c85e6faf843a20443c
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-11-14 15:08:23 -0500

    Fixed mapping convention error in names, added PropTypes to Movie-View

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,19 +1,41 @@
+import PropTypes from 'prop-types';
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
+      <div>{movie.ImageUrl}</div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.Director.Name}</span>
       </div>
       <div>
-      <span>Description: </span>
-      <span>{movie.description}</span>
-    </div>
-    <button onClick={onBackClick}>Back</button>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    ImageUrl: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }).isRequired,
+    Description: PropTypes.string.isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };
