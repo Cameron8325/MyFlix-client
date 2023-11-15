961c72f533d17795eb4db4c5427dd836f4601326
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2023-11-15 17:58:41 -0500

    Cleared localStorage

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -62,7 +62,7 @@ export const MainView = () => {
             {movie.Title}
           </div>
         ))}
-        <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
     </div>
   );
 };
